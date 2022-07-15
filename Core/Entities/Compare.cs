10c30343d5fb2aeaa583e449222b4f53f5db74b5
Emre GÜNDOĞDU <emre26gundogdu@gmail.com>
2022-07-15 20:15:05 +0300

    compare & productCompare entities added

---
+++ Core/Entities/Compare.cs
@@ -0,0 +1,34 @@
+﻿using Core.Entities.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Entities
+{
+    public class Compare:Entity
+    {
+        public string Username { get; set; }
+        public List<ProductCompare> ProductCompares { get; set; } = new List<ProductCompare>();
+        public void AddItem(int productId)
+        {
+            var existingItem = ProductCompares.FirstOrDefault(x=>x.ProductId == productId);
+            if (existingItem!=null)
+                return;
+            ProductCompares.Add(new ProductCompare
+            {
+                ProductId = productId,
+                CompareId = this.Id
+            });
+        }
+        public void RemoveItem(int productId)
+        {
+            var removedItem = ProductCompares.FirstOrDefault(x=>x.ProductId == productId);
+            if (removedItem !=null)
+            {
+                ProductCompares.Remove(removedItem);
+            }
+        }
+    }
+}
